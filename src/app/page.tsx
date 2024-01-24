4a5bf68b5c3ac6310e385f74827c5d62f439d73b
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-01-24 00:04:04 -0300

    feat: estruturando o layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-900 text-slate-100 p-4">
-      <h1 className="font" >Olá tailwind</h1>
-    </div>
-  );
+  return <h1>hello world</h1>
 }
