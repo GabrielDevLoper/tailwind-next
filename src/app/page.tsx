0d9e7a6a9e4d6c0452355fdf24c937f5c3054a8d
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-01-24 00:04:04 -0300

    feat: configurando prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-slate-900 text-slate-100 p-4">
-      <h1 className="font" >Olá tailwind</h1>
+    <div className="h-screen bg-slate-100 p-4 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+      <h1 className="text-purple-500">Olá tailwind</h1>
     </div>
-  );
+  )
 }
