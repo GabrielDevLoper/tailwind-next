32e8e67df3c36f762b0b8e8d5e8fa0f18afde09b
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-01-30 15:19:02 -0300

    feat: logo e input de busca do layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
-<<<<<<< HEAD
   return <h1>hello world</h1>
-=======
+
   return (
     <div className="h-screen bg-slate-100 p-4 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
       <h1 className="text-purple-500">Olá tailwind</h1>
     </div>
   )
->>>>>>> 0d9e7a6a9e4d6c0452355fdf24c937f5c3054a8d
 }
