4e3fbfd9d8dc15ab38dce4e39d03ec9b9ef9686f
Gabriel Barreto <gabriel.barreto@tjal.jus.br>
2024-06-25 11:07:05 -0300

    feat: progress

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,10 @@
-import { Search } from 'lucide-react'
+import { BarChart, CheckSquare, Cog, Home, LifeBuoy, Search, SquareStack, Users } from 'lucide-react'
 import { Logo } from './Logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './MainNavigation/NavItem'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2  shadow-sm">
@@ -16,7 +16,22 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+          <NavItem title="Home" icon={Home}/>
+          <NavItem title="Dashboard" icon={BarChart}/>
+          <NavItem title="Projects" icon={SquareStack}/>
+          <NavItem title="Tasks" icon={CheckSquare}/>
+          <NavItem title="Users" icon={Users}/>
+      </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+           <NavItem title="Support" icon={LifeBuoy}/>
+           <NavItem title="Settings" icon={Cog}/>
+        </nav>
+        <div className="flex flex-col gap-4 rounded-large bg-violet-50 px-4 py-5">
+          
+        </div>
+      </div>
     </aside>
   )
 }
